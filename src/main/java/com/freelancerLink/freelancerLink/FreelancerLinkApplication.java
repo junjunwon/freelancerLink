1a2567022b7d6134a8010eef0c1e7aee63ca6947
junjunwon <wnsgh1993@gmail.com>
2021-08-10 00:37:38 +0900

    commit test

--- src/main/java/com/freelancerLink/freelancerLink/FreelancerLinkApplication.java
+++ src/main/java/com/freelancerLink/freelancerLink/FreelancerLinkApplication.java
@@ -8,6 +8,7 @@ public class FreelancerLinkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FreelancerLinkApplication.class, args);
+		System.out.print("");
 	}
 
 }
