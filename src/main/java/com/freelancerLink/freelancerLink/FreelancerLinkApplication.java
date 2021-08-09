470ff78e2980969946a760f778258b1d13b9475a
junjunwon <wnsgh1993@gmail.com>
2021-08-10 00:08:48 +0900

    Initial commit

---
+++ src/main/java/com/freelancerLink/freelancerLink/FreelancerLinkApplication.java
@@ -0,0 +1,13 @@
+package com.freelancerLink.freelancerLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancerLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelancerLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freelancerLink/freelancerLink/FreelancerLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freelancerLink.freelancerLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancerLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
