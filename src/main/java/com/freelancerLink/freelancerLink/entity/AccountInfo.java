81e45ec1429c1d3bf528b45cc3737e03e4e801bc
junjunwon <wnsgh1993@gmail.com>
2021-08-12 16:58:53 +0900

    테스트 계정정보 class 추가.

---
+++ src/main/java/com/freelancerLink/freelancerLink/entity/AccountInfo.java
@@ -0,0 +1,14 @@
+package com.freelancerLink.freelancerLink.entity;
+
+public class AccountInfo {
+    private String name;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
