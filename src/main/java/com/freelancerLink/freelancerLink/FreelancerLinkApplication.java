78a0f395fc068e0a2cc1d813df95bb26641ef8c6
junjunwon <wnsgh1993@gmail.com>
2021-08-12 16:59:51 +0900

    @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})로 datasource를 배제 -> 프로퍼티에 DB경로 미입력으로 인한 오류 방지.

--- src/main/java/com/freelancerLink/freelancerLink/FreelancerLinkApplication.java
+++ src/main/java/com/freelancerLink/freelancerLink/FreelancerLinkApplication.java
@@ -2,12 +2,13 @@ package com.freelancerLink.freelancerLink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class FreelancerLinkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FreelancerLinkApplication.class, args);
-		System.out.print("");
 	}
 }
