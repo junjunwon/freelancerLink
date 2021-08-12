fd9218a6ddc5446d608eee90e407894089669a23
junjunwon <wnsgh1993@gmail.com>
2021-08-12 17:45:06 +0900

    DB에 등록된 collection 인 AccountInfo로 변경.

--- src/main/java/com/freelancerLink/freelancerLink/service/AccountService.java
+++ src/main/java/com/freelancerLink/freelancerLink/service/AccountService.java
@@ -12,13 +12,15 @@ import java.util.concurrent.ExecutionException;
 @Service
 public class AccountService {
 
-    private static final String COLLECTION_NAME = "accounts";
+    private static final String COLLECTION_NAME = "AccountInfo"; //firestore collection은 table과 비슷한 개념.
 
     public String saveAccountInfo(AccountInfo account) throws ExecutionException, InterruptedException {
+
         Firestore dbFirestore = FirestoreClient.getFirestore();
 
         ApiFuture<WriteResult> collectionApiFuture = dbFirestore.collection(COLLECTION_NAME).document(account.getName()).set(account);
 
         return collectionApiFuture.get().getUpdateTime().toString();
+
     }
 }
