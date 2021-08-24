367367264192477d7aadf24a6785ca411f9f2de2
junjunwon <wnsgh1993@gmail.com>
2021-08-24 13:40:38 +0900

    파이어베이스 삭제.

--- src/main/java/com/freelancerLink/freelancerLink/service/AccountService.java
+++ src/main/java/com/freelancerLink/freelancerLink/service/AccountService.java
@@ -1,10 +1,6 @@
 package com.freelancerLink.freelancerLink.service;
 
 import com.freelancerLink.freelancerLink.entity.AccountInfo;
-import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.WriteResult;
-import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.ExecutionException;
@@ -15,12 +11,6 @@ public class AccountService {
     private static final String COLLECTION_NAME = "AccountInfo"; //firestore collection은 table과 비슷한 개념.
 
     public String saveAccountInfo(AccountInfo account) throws ExecutionException, InterruptedException {
-
-        Firestore dbFirestore = FirestoreClient.getFirestore();
-
-        ApiFuture<WriteResult> collectionApiFuture = dbFirestore.collection(COLLECTION_NAME).document(account.getName()).set(account);
-
-        return collectionApiFuture.get().getUpdateTime().toString();
-
+        return "finish";
     }
 }
