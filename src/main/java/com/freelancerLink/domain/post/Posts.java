7eb132cfea3a710780fef4b98cf03b6f8a162359
junjunwon <wnsgh1993@gmail.com>
2021-08-27 12:52:03 +0900

    JPA로 H2디비 연결 및 테스트

---
+++ src/main/java/com/freelancerLink/domain/post/Posts.java
@@ -0,0 +1,41 @@
+package com.freelancerLink.domain.post;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder //해당 클래스의 빌더 패턴 클래스 생성, 생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함됌.
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    /**
+     * setter의 무작정 생성 제한
+     * 차후 기능 변경 시 해당 클래스의 인스턴스 값들이 언제 어디서 변해야 하는지 코드상으로 명확히 구분할 수가 없다.
+     * Entity 클래스에서는 절대 Setter 메소드 생성 X -> 대신, 해당 필드의 값 변경이 필요하면 명확히 그 목적과 의도를 나타낼 수 있는 메소드를 추가한다.
+     * public void cancelPost(){
+     *     this.status = false;
+     * }
+     */
+}
---
+++ src/main/java/com/freelancerLink/domain/post/PostsRepository.java
@@ -0,0 +1,18 @@
+package com.freelancerLink.domain.post;
+
+import com.freelancerLink.domain.post.Posts;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    /**
+     * JpaRepository : DB Layer 접근자.
+     * JpaRepository<Entity class, PK tyle>를  상속하면 기본적인 CRUD 메소드가 자동 생성된다.
+     * @Repository를 추가할 필요도 없다.
+     *   - 주의할점은 Entitiy 클래스와 기본 Entity Repository는 함께 위치해야하는 점!
+     *   - Entity 클래스는 기본 Repository 없이는 제대로 역할을 할 수 없다.
+     *   - 프로젝트 규모가 커져 도메인 별로 프로젝트를 분리해야할 때 Entity class와 기본 Repository는 함께 움직여야 해서 도메인 패키지에서 함께 관리한다.
+     */
+}
